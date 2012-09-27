3d285895a82d4fdd729d4cb28ec408f62408bd21
nepbrosincb <nepbrosinc@hotmail.com>
2012-09-26 22:48:31 -0400

    Initial Solution Structure

---
+++ ApartmentExpenseManager.BusinessLogic.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ApartmentExpenseManager.BusinessLogic.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
